a63ce107c5c67c014d70956e30b4fa8e74625054
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-10-03 16:57:39 +0800

    Update Target.java

--- copy/03_itheima_spring_aop/itheima_spring_aop/src/main/java/com/itheima/No_02_aop/Target.java
+++ copy/03_itheima_spring_aop/itheima_spring_aop/src/main/java/com/itheima/No_02_aop/Target.java
@@ -3,6 +3,6 @@ package com.itheima.No_02_aop;
 public class Target implements TargetInterface {
     public void save() {
         System.out.println("save running.....");
-        int i = 1/0;
+//        int i = 1/0;
     }
 }
